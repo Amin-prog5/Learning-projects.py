27d1f6743519eee752659a2b014bc232deab194f
Amin <155407117+Amin-prog5@users.noreply.github.com>
2025-08-25 13:09:59 +0300

    Ai-part.py

--- Ai-part.py
+++ Ai-part.py
@@ -1,10 +1,10 @@
 import face_recognition
 import cv2
 import numpy as np
+
 #import requests
 
 video_capture = cv2.VideoCapture(0)
-
 
 bill_image = face_recognition.load_image_file("C:/Users/Amin/Desktop/PythonProject/bill.jpg")
 bill_face_encoding = face_recognition.face_encodings(bill_image)[0]
@@ -28,6 +28,8 @@ andy_image = face_recognition.load_image_file("Andy Samberg0_429.jpg")
 andy_face_encoding = face_recognition.face_encodings(andy_image)[0]
 
 
+
+
 known_face_encodings = [
     bill_face_encoding,
     Tom_face_encoding,
@@ -35,7 +37,8 @@ known_face_encodings = [
     henry_face_encoding,
     amin_face_encoding,
     aziz_face_encoding,
-andy_face_encoding
+    andy_face_encoding,
+
 ]
 known_face_names = [
     "bill",
@@ -45,21 +48,31 @@ known_face_names = [
     "amin",
     "aziz",
     "andy",
+   
+
 
 ]
 
+id = {
+    "bill": 0,
+    "Tom": 1,
+    "holland": 2,
+    "henry": 3,
+    "amin": 4,
+    "aziz": 5,
+    "andy": 6,
+    
 
+}
 
 #ESP32_IP = "http://192.168.1.166"
 
 #def send_to_esp32(result):
- #   if result == "unknown":
-  #          requests.get(f"{ESP32_IP}/deny")
+#   if result == "unknown":
+#          requests.get(f"{ESP32_IP}/deny")
 
-   # else:
-    #        requests.get(f"{ESP32_IP}/open")
-
-
+# else:
+#        requests.get(f"{ESP32_IP}/open")
 
 
 process_this_frame = True
@@ -68,14 +81,11 @@ while True:
 
     ret, frame = video_capture.read()
 
-
     if process_this_frame:
 
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-
         rgb_small_frame = small_frame[:, :, ::-1]
-
 
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
@@ -86,43 +96,33 @@ while True:
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
             name = "unknown"
 
-
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
 
-
-          #  send_to_esp32(name)
+            #  send_to_esp32(name)
 
             face_names.append(name)
 
     process_this_frame = not process_this_frame
 
-
     for (top, right, bottom, left), name in zip(face_locations, face_names):
-
         top *= 4
         right *= 4
         bottom *= 4
         left *= 4
 
-
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
 
         cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-
+        cv2.putText(frame, name + " " + str(id.get(name, 0)), (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+       # print(str(id.get(name, 0)))
     cv2.imshow('vid', frame)
-
 
     if cv2.waitKey(1) & 0xFF == ord('e'):
         break
 
-
 video_capture.release()
 cv2.destroyAllWindows()
-
